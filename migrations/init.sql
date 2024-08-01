89fd2ce54fc125d7ee087bf515692bdc5673b554
Santi Lertsumran <santi.lertsumran@gmail.com>
2024-08-01 21:58:21 +0700

    feat: add default to created_at and updated_at

--- migrations/init.sql
+++ migrations/init.sql
@@ -2,21 +2,21 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TYPE valid_status AS ENUM ('TODO', 'DOING', 'DONE');
 
-CREATE TABLE tasks(
+CREATE TABLE IF NOT EXISTS tasks(
   id uuid DEFAULT uuid_generate_v4(),
   content VARCHAR(50),
   status valid_status,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW(),
   deleted_at TIMESTAMP
 );
 
-CREATE TABLE users(
+CREATE TABLE IF NOT EXISTS users(
   id uuid DEFAULT uuid_generate_v4(),
   email VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
   is_verified BOOLEAN DEFAULT false,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP,
+  created_at TIMESTAMP DEFAULT NOW(),
+  updated_at TIMESTAMP DEFAULT NOW(),
   deleted_at TIMESTAMP
 );
